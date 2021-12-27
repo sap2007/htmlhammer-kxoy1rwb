8c96371465971dd1e64db01ad48505d51b0086da
sap2007 <96510763+undefined@users.noreply.github.com>
2021-12-27 17:20:39 +0000

    deploy Mon Dec 27 2021 17:20:39 GMT+0000 (Coordinated Universal Time)

---
+++ dist/index.js
@@ -0,0 +1,53 @@
+import { packd_export_0 } from 'https://srv.divriots.com/packd/htmlhammer';const { style,button,span,customElement } = packd_export_0;;
+const MyCounterStyle = style(
+/*css*/
+`
+      * {
+        font-size: 200%;
+      }
+
+      span {
+        width: 4rem;
+        display: inline-block;
+        text-align: center;
+      }
+
+      button {
+        width: 4rem;
+        height: 4rem;
+        border: none;
+        border-radius: 10px;
+        background-color: seagreen;
+        color: white;
+      }
+    `);
+const MyCounter = customElement("my-counter", {
+  Count: 0,
+  CounterDisplay: null,
+
+  connectedCallback() {
+    this.attachShadow({
+      mode: "open"
+    });
+    this.shadowRoot.append(MyCounterStyle, button({
+      id: "dec",
+      onclick: this.Dec
+    }, "-"), this.CounterDisplay = span(this.Count), button({
+      id: "inc",
+      onclick: this.Inc
+    }, "+"));
+  },
+
+  Inc() {
+    this.Update(++this.Count);
+  },
+
+  Dec() {
+    this.Update(--this.Count);
+  },
+
+  Update(count) {
+    this.CounterDisplay.textContent = count;
+  }
+
+});
